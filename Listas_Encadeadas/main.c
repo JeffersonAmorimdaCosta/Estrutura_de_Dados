5525b3f25ef9c562227fbbcd4aeb872935e9a5a6
matos-ayrton <ayrtonsilvamatos@gmail.com>
2023-10-27 09:39:25 -0300

    Alteração na Main

--- Listas_Encadeadas/main.c
+++ Listas_Encadeadas/main.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include "listadinalu.h"
+#include "listadinalu.c"
 
 int main(void){ // Testes:
     StudentList alunos;
@@ -13,35 +13,35 @@ int main(void){ // Testes:
         system("cls");
         printf("Editor de lista encadeada - Atividade 1\n");
         printf("---------------------------------------\n");
-        printf("0 - Encerrar programa");
+        printf("0 - Encerrar programa\n");
         printf("1 - Criar lista\n");
         printf("2 - Verificação da lista (vazia)\n");
         printf("3 - Tamanho da lista\n");
-        printf("4 - Obter dados pela posição\n");
-        printf("5 - Obter posição pela matrícula\n");
+        printf("4 - Obter dados pela posicao\n");
+        printf("5 - Obter posição pela matricula\n");
         printf("6 - Inserir aluno no final da lista\n");
-        printf("7 - Remover pela matrícula\n");
-        printf("8 - Exibição dos dados\n");
-        printf("9 - Inserir aluno na posição\n");
+        printf("7 - Remover pela matricula\n");
+        printf("8 - Exibicao dos dados\n");
+        printf("9 - Inserir aluno na posicao\n");
         printf("10 - Inserir aluno de forma crescente\n");
-        printf("11 - Remover na posição\n");
-        printf("12 - Pesquisar pela matrícula ou nome\n\n");
-        printf("Escolha uma opção: ");
-        scanf("%hd", opcao);
+        printf("11 - Remover na posicaoo\n");
+        printf("12 - Pesquisar pela matricula ou nome\n\n");
+        printf("Escolha uma opcao: ");
+        scanf("%hd", &opcao);
 
         switch(opcao) {
             case 0:
-                printf("Programa encerrado pelo usuário.");
+                printf("Programa encerrado pelo usuario.");
                 exit(0);
             case 1:
                 create(&alunos);
                 break;
             case 2:
                 if(empty(&alunos) == 1) {
-                    printf("\n\nSua lista está vazia.");
+                    printf("\n\nSua lista esta vazia.");
                 }
                 else if(empty(&alunos) == 0) {
-                    printf("\n\nSua lista não está vazia.");
+                    printf("\n\nSua lista nao esta vazia.");
                 }
                 break;
             case 3:
