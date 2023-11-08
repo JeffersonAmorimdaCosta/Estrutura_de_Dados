1b357dd0a647b2c09892ead12fa5fb9cd1c6e139
matos-ayrton <131035068+matos-ayrton@users.noreply.github.com>
2023-11-08 10:52:50 -0300

    filapessoas.c finalizada.

--- Filas/Questao2/filapessoas.c
+++ Filas/Questao2/filapessoas.c
@@ -1,134 +1,124 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "filapessoas.h"
 #include "filaprioridade.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-void start_Queue(Queue *fila) {
-    fila->head = fila->end = NULL;
+void start_Queue(Queue *filaNormal, Queue *filaIdosos, Queue *filaGestantes) {
+    filaNormal->head = filaNormal->end = NULL;
+    filaIdosos->head = filaIdosos->end = NULL;
+    filaGestantes->head = filaGestantes->end = NULL;
 }
 
-int size_queue(Queue *queueNormal, Queue *queueAge, Queue *queuePreg){
-    int cont = 0, contTotal = 0;
+int size_queue(Queue *queueNormal, Queue *queueAge, Queue *queuePreg) {
+    int contNormal = 0, contTotal = 0;
 
-    Node *ast = queueNormal->head;
-    while (ast != NULL){ // Percorre a fila de pedido e faz a contagem
-        cont ++;
-        ast = ast->next;
+    int contIdosos = size_age(queueAge); 
+    int contGestantes = size_preg(queuePreg);
+
+    Node *aux = queueNormal->head;
+    while(aux != NULL) {
+        contNormal++;
+        aux = aux->next;
     }
-
-    contTotal = cont + size_Queue_age(queueAge) + size_Queue_preg(queuePreg);
+    contTotal = (contNormal + contGestantes + contIdosos);
 
     return contTotal;
 }
 
-void display(Queue *fila, Queue *filaIdosos, Queue *filaGestantes) {
-    if (size_queue(fila, filaIdosos, filaGestantes) == 0){
-        printf("A lista esta vazia\n");
-    }
+int insert_Queue(Queue *filaNormal, Queue *filaIdosos, Queue *filaGestantes, Person p) {
 
-    else{
-        if(size_Queue_preg(filaGestantes) > 0){
-            display_by_Preg(filaGestantes);
-            display_by_age(filaIdosos);
-            Node *ast = fila->head;
-            while (ast != NULL){ // Percorre a fila de pedido e exibe
-                printf("%s\n", ast->person->name);
-                ast = ast->next;
-            }
+    for(int i = 0; p.isPregnant[i]; i++) {
+      p.isPregnant[i] = toupper(p.isPregnant[i]);
+    }
+  
+    if(strcmp(p.isPregnant, "SIM") == 0) {
+        if(insert_preg(filaGestantes, p) == 1) {
+            return 1;
         }
-        else if(size_Queue_preg(filaGestantes) == 0 && size_Queue_age(filaIdosos) > 0) {
-            display_by_age(filaIdosos);
-            Node *ast = fila->head;
-            while (ast != NULL){ // Percorre a fila de pedido e exibe
-                printf("%s\n", ast->person->name);
-                ast = ast->next;
-            }
+    }
+    if(p.age >= 65) {
+        if(insert_age(filaIdosos, p) == 1) {
+            return 1;
         }
-        else if(size_Queue_preg(filaGestantes) == 0 && size_Queue_age(filaIdosos) == 0) {
-            Node *ast = fila->head;
-            while (ast != NULL){ // Percorre a fila de pedido e exibe
-                printf("%s\n", ast->person->name);
-                ast = ast->next;
-            }
+    }
+    if (strcmp(p.isPregnant, "NAO") == 0 && p.age < 65) {
+        Node *new_node = (Node*) malloc(sizeof(Node));
+
+        if(new_node == NULL) {
+            return 0;
         }
+
+        strcpy(new_node->person.name, p.name);
+        strcpy(new_node->person.isPregnant, p.isPregnant);
+        new_node->person.age = p.age;
+        new_node->person.cpf = p.cpf;
+
+        if (size_queue(filaNormal, filaIdosos, filaGestantes) == 0) {
+            filaNormal->head = filaNormal->end = new_node;
+            return 1;
+        }
+
+        filaNormal->end->next = new_node;
+        filaNormal->end = new_node;
+        return 1;
     }
 }
 
 void display_first(Queue *fila, Queue *filaIdosos, Queue *filaGestantes) {
-    if(size_Queue_preg(filaGestantes) == 0) {
-        if(size_Queue_age(filaIdosos) == 0) {
-            printf("CPF: %d", fila->head->person->cpf);
-            printf("Nome: %s", fila->head->person->name);
-            printf("Idade: %d", fila->head->person->age);
-            printf("Gestante: %s", fila->head->person->isPregnant);
-        }
-        else if(size_Queue_age(filaIdosos) > 0) {
-            printf("CPF: %d", filaIdosos->head->person->cpf);
-            printf("Nome: %s", filaIdosos->head->person->name);
-            printf("Idade: %d", filaIdosos->head->person->age);
-            printf("Gestante: %s", filaIdosos->head->person->isPregnant);            
-        }
-    }
-    else if(size_Queue_preg(filaGestantes) > 0) {
-            printf("CPF: %d", filaGestantes->head->person->cpf);
-            printf("Nome: %s", filaGestantes->head->person->name);
-            printf("Idade: %d", filaGestantes->head->person->age);
-            printf("Gestante: %s", filaGestantes->head->person->isPregnant);    
-    }
+  if(size_preg(filaGestantes) > 0) {
+    printf("CPF: %ld\n", filaGestantes->head->person.cpf);
+    printf("Nome: %s\n", filaGestantes->head->person.name);
+    printf("Idade: %d\n", filaGestantes->head->person.age);
+    printf("Gestante: %s\n", filaGestantes->head->person.isPregnant);
+  }
+  else if(size_age(filaIdosos) > 0) {
+    printf("CPF: %ld\n", filaIdosos->head->person.cpf);
+    printf("Nome: %s\n", filaIdosos->head->person.name);
+    printf("Idade: %d\n", filaIdosos->head->person.age);
+    printf("Gestante: %s\n", filaIdosos->head->person.isPregnant);
+  }
+  else if(size_queue(fila, filaIdosos, filaGestantes) > 0) {
+    printf("CPF: %ld\n", fila->head->person.cpf);
+    printf("Nome: %s\n", fila->head->person.name);
+    printf("Idade: %d\n", fila->head->person.age);
+    printf("Gestante: %s\n", fila->head->person.isPregnant);
+  }
 }
 
-int insert_Queue(Queue *fila, Queue *filaIdosos, Queue *filaGestantes, Person p) {
-    Node *new_node = (Node *) malloc(sizeof(Node)); // Cria o novo nó
+void display(Queue *filaNormal, Queue *filaIdosos, Queue *filaGestantes) {
+  int counter = 1;
+  Node *aux;
 
-    if (new_node == NULL){ // Verifica se alocou memoria
-        return 0;
-    }
-
-    if(new_node->person->isPregnant[0] == 'S' || new_node->person->isPregnant[0] == 's') {
-        insert_by_preg(filaGestantes, p);
-    }
-    if(new_node->person->age >= 65) {
-        insert_by_age(filaIdosos, p);
-    }
-
-    else {
-        strcpy(new_node->person->name, p.name); // Atribui o nome o novo nome recebido com parametro
-        strcpy(new_node->person->isPregnant, p.isPregnant);
-        new_node->person->age = p.age;
-        new_node->person->cpf = p.cpf;
-        new_node->next = NULL; // Define o proximo no como null
-    }
-
-    if (size_queue(fila, filaIdosos, filaGestantes) == 0 && size_Queue_age(filaIdosos) == 0 && size_Queue_preg(filaGestantes) == 0){ // Verifica se está a fila está vazia
-        fila->head = fila->end = new_node; // Define o inicio e o fim como o novo nó
-        return 1;
-    }
-
-    else{ // Se não estiver vazia
-        fila->end->next = new_node;
-        fila->end = new_node;
-        return 1;
-    }
+  for(aux = filaGestantes->head; aux != NULL; aux = aux->next, counter++) {
+    printf("%d. %s\n", counter, aux->person.name);
+  }
+  for(aux = filaIdosos->head; aux != NULL; aux = aux->next, counter++) {
+    printf("%d. %s\n", counter, aux->person.name);
+  }
+  for(aux = filaNormal->head; aux != NULL; aux = aux->next, counter++) {
+    printf("%d. %s\n", counter, aux->person.name);
+  }
 }
 
-int remove_Queue(Queue *fila, Queue *filaIdosos, Queue *filaGestantes) {
-    if (size_queue(fila, filaGestantes, filaIdosos) == 0){
-        return 0;
+int remove_queue(Queue *fila, Queue *filaIdosos, Queue *filaGestantes) {
+  if(size_preg(filaGestantes) > 0) {
+    if(remove_preg(filaGestantes) == 1) {
+        return 1;
     }
-
-    if(size_Queue_preg(filaGestantes) == 0) {
-        if(size_Queue_age(filaIdosos) == 0) {
-            Node *temp = fila->head; // Variável assistente
-            fila->head = fila->head->next;
-            free(temp);
-            return 1;
-        }
-        else if((size_Queue_age(filaIdosos)) > 0) {
-            remove_by_age(filaIdosos);
-        }
+  }
+  else if(size_age(filaIdosos) > 0) {
+    if(remove_age(filaIdosos) == 1) {
+        return 1;
     }
-    else if(size_Queue_preg(filaGestantes) > 0) {
-        remove_by_preg(filaGestantes);
+  }
+  else if(size_age(filaIdosos) == 0 && size_preg(filaGestantes) == 0) {
+    if (size_queue(fila, filaIdosos, filaGestantes) == 0) {
+      return 0;
     }
+    Node *temp = fila->head;
+    fila->head = fila->head->next;
+    free(temp);
+    return 1;
+  }
 }
