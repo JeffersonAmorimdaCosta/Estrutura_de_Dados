5a1daac661cdece44d30428f945025c499f331f1
matos-ayrton <131035068+matos-ayrton@users.noreply.github.com>
2023-11-08 10:55:06 -0300

    filaprioridade.h finalizada.

--- Filas/Questao2/filaprioridade.h
+++ Filas/Questao2/filaprioridade.h
@@ -1,10 +1,8 @@
 #include "filapessoas.h"
 
-int insert_by_age(Queue *filaprIdosos, Person p);
-int remove_by_age(Queue *filaprIdosos);
-int insert_by_preg(Queue *filaprGestantes, Person p);
-int remove_by_preg(Queue *filaprGestantes);
-void display_by_age(Queue *filaprIdosos);
-void display_by_Preg(Queue *filaprGestantes);
-int size_Queue_age(Queue *filaprIdosos);
-int size_Queue_preg(Queue *filaprGestantes);
+int size_preg(Queue *queuePregnant); 
+int size_age(Queue *queueAge);
+int insert_preg(Queue *queuePreg, Person p);
+int insert_age(Queue *queueAge, Person p);
+int remove_preg(Queue *queuePreg);
+int remove_age(Queue *queueAge);
