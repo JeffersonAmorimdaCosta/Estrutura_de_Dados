47ea8240647f3c2753f942c5cde39354b4e94fe3
matos-ayrton <131035068+matos-ayrton@users.noreply.github.com>
2023-11-08 10:53:22 -0300

    filapessoas.h finalizada.

--- Filas/Questao2/filapessoas.h
+++ Filas/Questao2/filapessoas.h
@@ -1,12 +1,12 @@
 typedef struct person {
     char name[30];
-    int cpf;
+    long cpf;
     int age;
     char isPregnant[3];
 } Person;
 
 typedef struct node{
-    Person person[30];
+    Person person;
     struct node *next;
 } Node;
 
@@ -15,9 +15,9 @@ typedef struct queue {
     Node *end;
 } Queue;
 
-void start_Queue(Queue *fila); // Cria a fila
+void start_Queue(Queue *filaNormal, Queue *filaIdosos, Queue *filaGestantes); // Cria a fila
 void display(Queue *fila, Queue *filaIdosos, Queue *filaGestantes); // Mostra a fila
 void display_first(Queue *fila, Queue *filaIdosos, Queue *filaGestantes);
 int size_queue(Queue *queueNormal, Queue *queueAge, Queue *queuePreg); // Mostra o tamanho da fila
 int insert_Queue(Queue *fila, Queue *filaIdosos, Queue *filaGestantes, Person p); // Insere na fila
-int remove_Queue(Queue *fila, Queue *filaIdosos, Queue *filaGestantes); // Remove da fila
+int remove_queue(Queue *fila, Queue *filaIdosos, Queue *filaGestantes); // Remove da fila
