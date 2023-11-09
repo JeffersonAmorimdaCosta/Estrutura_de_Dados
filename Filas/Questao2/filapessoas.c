40b0b853b7ff516c75e3359870cd0604a8e6c747
matos-ayrton <ayrtonsilvamatos@gmail.com>
2023-11-09 14:28:50 -0300

    Adição de comentários e correção de funções

--- Filas/Questao2/filapessoas.c
+++ Filas/Questao2/filapessoas.c
@@ -5,6 +5,10 @@
 #include <ctype.h>
 
 void start_Queue(Queue *filaNormal, Queue *filaIdosos, Queue *filaGestantes) {
+    /*
+    Explicação: aqui as filas serã inicializadas, sendo declarados seus inícios e finais como NULL e seus 
+    tamanhos como 0.
+    */
     filaNormal->head = filaNormal->end = NULL;
     filaIdosos->head = filaIdosos->end = NULL;
     filaGestantes->head = filaGestantes->end = NULL;
@@ -14,6 +18,10 @@ void start_Queue(Queue *filaNormal, Queue *filaIdosos, Queue *filaGestantes) {
 }
 
 int size_queue(Queue *queueNormal, Queue *queueAge, Queue *queuePreg) {
+    /*
+    Aqui, basicamente são utilizados os tamanhos armazenados nas filas, sendo somados e retornando
+    como uma só variável.
+    */
     int cont = 0;
 
     cont = queueNormal->tamanho + queueAge->tamanho + queuePreg->tamanho;
@@ -22,7 +30,15 @@ int size_queue(Queue *queueNormal, Queue *queueAge, Queue *queuePreg) {
 }
 
 int insert_Queue(Queue *filaNormal, Queue *filaIdosos, Queue *filaGestantes, Person p) {
-
+    /*
+    Aqui, primeiramente como eu não quero deixar o IF muito grande, coloquei já para toda a string ser 
+    maiúscula com o toupper. Segue o funcionamento do restante:
+    - No primeiro caso, a função analisará se a pessoa inserida é gestante. Caso seja, será utilizada a
+    função de inserir gestantes e, caso retorne 1, a inserção foi um sucesso. Isso se repetirá no segundo IF
+    onde caso não seja gestante, será analisado se a pessoa é idosa, entrando na função especificada para
+    inserção de idosos caso seja. Se não se enquadrar em nenhum dos casos, será inserido na fila normal, com
+    a rotina já conhecida. Ao fim, em todos os casos o tamanho da fila respectiva será incrementado.
+    */
     for(int i = 0; p.isPregnant[i]; i++) {
       p.isPregnant[i] = toupper(p.isPregnant[i]);
     }
@@ -66,13 +82,18 @@ int insert_Queue(Queue *filaNormal, Queue *filaIdosos, Queue *filaGestantes, Per
 }
 
 void display_first(Queue *fila, Queue *filaIdosos, Queue *filaGestantes) {
+  /*
+  A lógica dessa função é bem simples: se houver pessoas na fila de gestantes, imprime-se os dados do "head"
+  da fila de gestantes. Se não houverem gestantes, então será printado o "head" da fila de idosos, e caso os
+  primeiros casos não sejam verdadeiros, printará o "head" da fila normal.
+  */
   if(size_preg(filaGestantes) > 0) {
     printf("CPF: %s\n", filaGestantes->head->person.cpf);
     printf("Nome: %s\n", filaGestantes->head->person.name);
     printf("Idade: %d\n", filaGestantes->head->person.age);
     printf("Gestante: %s\n", filaGestantes->head->person.isPregnant);
   }
-  else if(size_age(filaIdosos) > 0) {
+  else if(size_age(filaIdosos) > 0 ) {
     printf("CPF: %s\n", filaIdosos->head->person.cpf);
     printf("Nome: %s\n", filaIdosos->head->person.name);
     printf("Idade: %d\n", filaIdosos->head->person.age);
@@ -87,6 +108,14 @@ void display_first(Queue *fila, Queue *filaIdosos, Queue *filaGestantes) {
 }
 
 void display(Queue *filaNormal, Queue *filaIdosos, Queue *filaGestantes) {
+  /*
+  Aqui, defini um counter para me ajudar a enumerar as pessoas da fila. Após isso, declarei uma
+  variável auxiliar para percorrer a fila. Então, para que pudesse de forma correta printar o necessário,
+  utilizei um for para percorrer todas as três listas, printando todos os nomes que estão dentro delas.
+  Para que estivesse tudo na ordem correta, primeiro foi a lista de gestantes, depois a de idosos e por fim
+  a normal.
+  */
+  
   int counter = 1;
   Node *aux;
 
@@ -102,6 +131,15 @@ void display(Queue *filaNormal, Queue *filaIdosos, Queue *filaGestantes) {
 }
 
 int remove_queue(Queue *fila, Queue *filaIdosos, Queue *filaGestantes) {
+  /*
+  Seguindo a lógica que fiz no display, aqui acontece o seguinte: se o tamanho da lista de gestantes
+  for maior que 0, então será removido o primeiro dessa fila através da função especificada para
+  essa fila, decrementando o tamanho da mesma.
+  Isso se aplicará para as seguintes: se não for gestante e tiver mais de 65 anos, o mesmo funcionamento
+  valerá para a fila de idosos, e caso não atenda nenhum dos dois casos, será feito a operação normal de
+  remoção na fila normal.
+  */
+
   if(size_preg(filaGestantes) > 0) {
     if(remove_preg(filaGestantes) == 1) {
         filaGestantes->tamanho--;
